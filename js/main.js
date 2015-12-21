3516e9129dbca898a7d8be828e8932b04a28eccb
Chris Martin <cmartin616@gmail.com>
2015-12-21 13:19:20 -0500

    validation

--- js/main.js
+++ js/main.js
@@ -10,11 +10,38 @@ $(function() {
       var answer3 = $('#question3answer').val();
       var answer4 = $('#question4answer').val();
 
-      var homepage = 'http://localhost/github/momsXmas/';
+      // var homepage = 'http://localhost/github/momsXmas/';
       // var homepage = 'http://chrismartingisdev.com/';
+      var homepage = 'http://cmartin.esri.com/github/momsXmas/';
 
       var link = homepage + answer1 + answer2 + answer3 + answer4 + '.html';
 
       $('#answerLink').attr('href', link);
+    });
+
+    $('#validateButton').on('click', function(){
+      var answers = [$('#question1answer'), $('#question2answer'), $('#question3answer'), $('#question4answer')];
+
+      var validations = [$('#question1validate'), $('#question2validate'), $('#question3validate'), $('#question4validate')];
+
+      var answersLength = answers.length;
+      var count = 0;
+
+      _.each(answers, function (ans, i){
+        if (ans.val() === validations[i].val()){
+          $('#' + ans.attr('id')).addClass('passValidate');
+          count += 1;
+        } else {
+          $('#' + ans.attr('id')).addClass('failValidate');
+        }
+      });
+
+      if (answersLength === count) {
+        var $answerLink = $('#answerLink');
+        $answerLink.removeClass('btn-danger');
+        $answerLink.addClass('btn-success');
+        $answerLink.removeAttr('disabled');
+      }
+
     });
 });
